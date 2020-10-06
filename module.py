0c7063899095ea4bf94824743b1600cf1fa609be
areeveg <areevegam@gmail.com>
2020-10-06 23:42:58 +0800

    update5

--- module.py
+++ module.py
@@ -75,16 +75,16 @@ class Root(Tk):
 
     def StartDecrypt(self):
         alpha = 0.45
-        vslfile = self.filePath
-        msgfile = self.filePath + '/message.txt' # can be any type of file
-        encfile = self.filePath +'/encoded.png'
-        msgfile_decoded = self.filePath + '/tmp.txt'
+        vslfile = 'C:/Users/areev/OneDrive/Documents/GitHub/bpcs/examples/vessel.png'
+        msgfile = 'C:/Users/areev/OneDrive/Documents/GitHub/bpcs/examples/message.txt' # can be any type of file
+        encfile = 'C:/Users/areev/OneDrive/Documents/GitHub/bpcs/examples/encoded.png'
+        msgfile_decoded = 'C:/Users/areev/OneDrive/Documents/GitHub/bpcs/examples/tmp.txt'
 
         #bpcs.capacity(vslfile, alpha) # check max size of message you can embed in vslfile
         bpcs.encode(vslfile, msgfile, encfile, alpha) # embed msgfile in vslfile, write to encfile
         bpcs.decode(encfile, msgfile_decoded, alpha) # recover message from encfile 
 
-        path = self.filepath
+        path = "C:/Users/areev/OneDrive/Documents/GitHub/bpcs/examples/"
 
         imgpath1 = path + "vessel.png"
         imgpath2 = path + "encoded.png"
